b5eae12668830377c74d86bc4366cec18689675f
Jonathan Morales <82234968+Git-JMO@users.noreply.github.com>
2022-04-11 21:02:33 -0400

    Add files via upload

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/schema.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/schema.sql
@@ -43,23 +43,48 @@ CREATE TABLE salaries (
 -- from_date date
 -- to_date date
 
-CREATE TABLE Dept_Emp (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+
+SELECT * FROM dept_emp;
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+DROP TABLE titles CASCADE;
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(20) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
 );
-
-SELECT * FROM departments;
+SELECT * FROM titles;
