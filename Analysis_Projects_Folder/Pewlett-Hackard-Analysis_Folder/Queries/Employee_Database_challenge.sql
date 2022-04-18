02447e30fd94477d7f3f47f0a99baf83133938cb
Jonathan Morales <82234968+Git-JMO@users.noreply.github.com>
2022-04-18 18:08:26 -0400

    Add files via upload

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -35,3 +35,25 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 --Table successfully exported to csv in Data Folder
+
+-- Deliverable #2
+
+-- Create a Mentorship Eligibility table 
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	dep.from_date,
+	dep.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS dep
+		ON (e.emp_no = dep.emp_no)
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (dep.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+--Table successfully exported to csv in Data Folder
